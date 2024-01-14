d3d5dd0d2daaa03cdc82204c0292c8455a26f0d6
Girthward <73715975+Girthward@users.noreply.github.com>
2024-01-14 19:51:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
-var status = 1;  // 1 - online, 7 - invisible
+var games = [227300, 4000, 284160, 270880, 271590, 1174180, 359550];  // Enter here AppIDs of the needed games
+var status = 7;  // 1 - online, 7 - invisible
 
 
 user = new steamUser();
